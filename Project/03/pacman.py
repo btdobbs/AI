56ab135b8a4f6243115ca84cbb7e80b5ccbe84b4
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2023-03-03 15:41:23 -0500

    Update pacman.py

--- Project/03/pacman.py
+++ Project/03/pacman.py
@@ -564,7 +564,7 @@ def readCommand(argv):
 
     # Fix the random seed
     if options.fixRandomSeed:
-        random.seed('cs188')
+        random.seed('cs-ai')
 
     # Choose a layout
     args['layout'] = layout.getLayout(options.layout)
